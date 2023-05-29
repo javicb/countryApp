f36a01cceaeedb0470ba9ad2e265336f7c6a71fc
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-05-30 01:25:22 +0200

    routerlink, routerlinkoptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    ContactPageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
