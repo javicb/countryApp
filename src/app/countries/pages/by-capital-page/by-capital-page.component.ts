19bef61f4512e22f3a7c31f3554296e17d3a6f6d
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-07-30 01:29:10 +0200

    feat: :construction: searchbox operation

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  searchByCapital(termino: string): void {
+    console.log({termino});
+  }
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -8,4 +8,9 @@ import { Component, Input } from '@angular/core';
 })
 export class SearchBoxComponent {
   @Input() placeholder: string = '';
+  @Output() onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
 }
