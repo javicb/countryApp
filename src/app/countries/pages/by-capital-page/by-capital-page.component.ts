5de10cc3948b4d2e9a63deef24f4ed45b4d0ac73
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-07-30 02:05:14 +0200

    feat: :construction: show countries by screen

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CountryService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,11 +10,13 @@ import { CountryService } from '../../services/countries.service';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
   constructor(private countryService: CountryService) { }
 
   searchByCapital(termino: string): void {
     this.countryService.searchCapital(termino).subscribe(
-      countries => console.log(countries)
+      countries => this.countries = countries
     );
   }
 }
