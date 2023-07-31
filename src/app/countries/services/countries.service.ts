0df565e7c7714bc21c052915f71429bb4fd5cc44
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-08-01 01:54:05 +0200

    refactor: :recycle: refactor service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -10,6 +10,16 @@ export class CountryService {
 
   constructor(private httpClient: HttpClient) { }
 
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(url)
+    .pipe(
+      catchError(err => {
+        console.log('Error en el servicio', err.message);
+        return of([])
+      })
+    );
+  }
+
   searchCountryByAlphCode(code: string): Observable<Country | null> {
     return this.httpClient.get<Country[]>(`${this.url}/alpha/${code}`)
     .pipe(
@@ -22,33 +32,15 @@ export class CountryService {
   }
 
   searchCapital(capital: string): Observable<Country[]> {
-    return this.httpClient.get<Country[]>(`${this.url}/capital/${capital}`)
-    .pipe(
-      catchError(err => {
-        console.log('Error en el servicio', err.message);
-        return of([])
-      })
-    );
+    return this.getCountriesRequest(`${this.url}/capital/${capital}`);
   }
 
   searchCountry(country: string): Observable<Country[]> {
-    return this.httpClient.get<Country[]>(`${this.url}/name/${country}`)
-    .pipe(
-      catchError(err => {
-        console.log('Error en el servicio', err.message);
-        return of([])
-      })
-    );
+    return this.getCountriesRequest(`${this.url}/name/${country}`);
   }
 
   searchRegion(region: string): Observable<Country[]> {
-    return this.httpClient.get<Country[]>(`${this.url}/region/${region}`)
-    .pipe(
-      catchError(err => {
-        console.log('Error en el servicio', err.message);
-        return of([])
-      })
-    );
+    return this.getCountriesRequest(`${this.url}/region/${region}`);
   }
 
 }
