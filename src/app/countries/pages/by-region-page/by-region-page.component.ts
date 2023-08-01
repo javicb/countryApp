d1063a7d563344f399b0ae059823ecc27f43f612
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-08-01 02:58:02 +0200

    refactor: :recycle: searchbyregion only search by 'Africa' , 'Americas', 'Asia', 'Europe', 'Oceania'

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -12,11 +14,14 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public activeRegion?: Region;
 
   constructor(private countryService: CountryService) { }
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
     this.isLoading = true;
+    this.activeRegion = region;
     this.countryService.searchRegion(region).subscribe(
       countries => {
         this.countries = countries;
