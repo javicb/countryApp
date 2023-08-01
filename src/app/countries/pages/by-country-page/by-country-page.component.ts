b8d322178c202a220af18f2e14ae9b0151d2c3f4
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-08-01 02:47:13 +0200

    add loader to the rest of the pages

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -11,12 +11,17 @@ import { CountryService } from '../../services/countries.service';
 export class ByCountryPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countryService: CountryService) { }
 
   searchByCountry(termino: string): void {
+    this.isLoading = true;
     this.countryService.searchCountry(termino).subscribe(
-      countries => this.countries = countries
+      countries => {
+        this.countries = countries;
+        this.isLoading = false;
+      }
     );
   }
 
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -11,12 +11,17 @@ import { CountryService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countryService: CountryService) { }
 
   searchByRegion(region: string): void {
+    this.isLoading = true;
     this.countryService.searchRegion(region).subscribe(
-      countries => this.countries = countries
+      countries => {
+        this.countries = countries;
+        this.isLoading = false;
+      }
     );
   }
 
