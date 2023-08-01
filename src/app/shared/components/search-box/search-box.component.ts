1d6e07d4c710303fb5d8d32142485d1bdb9da745
Javier Cruz <javier.cruz.bonoso@avanade.com>
2023-08-01 02:34:40 +0200

    feat: :sparkles: debounce on search

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,11 +7,28 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
   @Input() placeholder: string = '';
   @Output() onValue = new EventEmitter<string>();
 
+  private debouncer: Subject<string> = new Subject<string>();
+
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(500)
+    )
+    .subscribe(value => {
+      this.onValue.emit(value);
+    });
+  }
+
   emitValue(value: string): void {
     this.onValue.emit(value);
+  }
+
+  onKeyPress(search: string): void {
+    this.debouncer.next(search);
   }
 }
